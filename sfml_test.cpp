07539999f52ee1f9ec2d5b961f1ec608d01f0a67
Mad Vitaliy <omelchenko.vitaliy1@gmail.com>
2023-04-16 00:20:57 +0300

    init comit

---
+++ sfml_test.cpp
@@ -0,0 +1,12 @@
+﻿// sfml_test.cpp : Defines the entry point for the application.
+//
+
+#include "sfml_test.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
